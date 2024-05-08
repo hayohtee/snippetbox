29306fe9865f24d132b4abb3f1e614cb70801fae
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-08 20:43:29 +0100

    Update main.go
    Create a simple server with a home handler func that returns "Hello from SnippetBox" text whenever a user requests to the root path

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main()  {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from SnippetBox"))
 }
