5f072f8ac9dc613100018d31a7e6e6366eaa202e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-08 21:02:35 +0100

    Update main.go
    Fix home handler func
    Changed the home handler func from acting as a catch all for other url path, also handle page not found if the requested url path doesn't match the root url

--- main.go
+++ main.go
@@ -10,13 +10,17 @@ func main()  {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from SnippetBox"))
 }
 
