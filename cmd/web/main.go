a50bbedfc9b0a70866b36542747fce01f78a767f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-10 18:09:30 +0100

    Update main.go
    Create dsn flag to try and get the data source name as a command line argument with default value if no argument is specified.
    Create and test a mysql database connection and also check for errors.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:#hammed1234#@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDb(*dsn)
+	if err != nil {
+		errLog.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		errorLog: errLog,
 		infoLog: infoLog,
@@ -25,11 +35,22 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errLog.Fatal(err)
 }
 
 type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
+}
+
+func openDb(dsn string)(*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}	
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
