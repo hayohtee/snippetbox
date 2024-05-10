22e1590949c39a30c772b942f3681adec067c069
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-10 19:26:22 +0100

    Update home handler func
    Make the home handler use the SnippetModel to fetch the latest 10 snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"html/template"
+	// "html/template"
 	"net/http"
 	"strconv"
 
@@ -29,6 +29,16 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// if err != nil {
 	// 	app.serverError(w, err)
 	// }
+
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v", snippet)
+	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
