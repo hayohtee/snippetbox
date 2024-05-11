7014f2df1b7cd4f18de6107d7681eaf2f36526dd
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-11 14:35:44 +0100

    Create templates.go

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,10 +1,38 @@
 package main
 
-import "github.com/hayohtee/snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/hayohtee/snippetbox/internal/models"
+)
 
 // templateData is a type to act as the holding structure
 // for any dynamic data that we want to pass to our HTML templates.
 type templateData struct {
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
+}
+
+// Create an in memory cache for all the templates needed by the app
+func newTemplateCache()(map[string]*template.Template, error) {	
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		files := []string {
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+		cache[name] = ts
+	}
+	return cache, nil
 }
