9c841d96c4ecc6bf20d33212e406346b69e79e9b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-16 20:56:35 +0100

    Add tls config to the server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"github.com/alexedwards/scs/mysqlstore"
@@ -51,10 +52,13 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	tslConfig := &tls.Config{CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256}}
+
 	srv := &http.Server{
-		Addr:     *addr,
-		ErrorLog: errLog,
-		Handler:  app.routes(),
+		Addr:      *addr,
+		ErrorLog:  errLog,
+		Handler:   app.routes(),
+		TLSConfig: tslConfig,
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
