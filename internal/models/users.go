99efbcdf2cc8cd02d6b1896bb86d53e9d8a1e364
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-17 12:44:57 +0100

    Rename users_uc_email to users_wc_email

--- internal/models/users.go
+++ internal/models/users.go
@@ -37,7 +37,7 @@ func (m *UserModel) Insert(name, email, password string) error {
 	if err != nil {
 		var mySQLError *mysql.MySQLError
 		if errors.As(err, &mySQLError) {
-			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_wc_email") {
 				return ErrDuplicateEmail
 			}
 		}
