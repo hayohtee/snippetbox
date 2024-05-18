537cd81f43a877bbcada636fd84716c3e07285ba
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-18 15:17:37 +0100

    Write the code for Authenticate method

--- internal/models/users.go
+++ internal/models/users.go
@@ -50,7 +50,29 @@ func (m *UserModel) Insert(name, email, password string) error {
 // provided email address and password. This will return
 // the relevant user ID if they do or an error.
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
 }
 
 // Exists - check if a user exists with a specific ID.
