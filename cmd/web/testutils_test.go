ac3fecb086ac0880edad77c472d917c84ea9bf8c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-25 12:41:50 +0100

    Create testutils_test.go to hold various utilities functions for performing end-to-end testing

---
+++ cmd/web/testutils_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type testServer struct {
+	*httptest.Server
+}
+
+func newTestApplication() *application {
+	return &application{
+		infoLog:  log.New(io.Discard, "", 0),
+		errorLog: log.New(io.Discard, "", 0),
+	}
+}
+
+func newTestServer(h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	return &testServer{ts}
+}
+
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(rs.Body)
+
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+	return rs.StatusCode, rs.Header, string(body)
+}
