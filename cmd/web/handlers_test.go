80a3b618ee0ac0443524b50f8a8774d5ca47b4a4
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-25 12:44:38 +0100

    Update TestPing to use the test_utils helper methods

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,39 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"github.com/hayohtee/snippetbox/internal/assert"
-	"io"
-	"log"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
-	ts := httptest.NewTLSServer(app.routes())
+	app := newTestApplication()
+	ts := newTestServer(app.routes())
 	defer ts.Close()
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	assert.Equal(t, http.StatusOK, rs.StatusCode)
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			t.Fatal(err)
-		}
-	}(rs.Body)
-
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	body = bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	statusCode, _, body := ts.get(t, "/ping")
+	assert.Equal(t, statusCode, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
