565a35891fbdb323b61e98a9b1f414f46e6eadd5
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-30 11:45:14 +0100

    Create users_test.go

---
+++ internal/models/users_test.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"github.com/hayohtee/snippetbox/internal/assert"
+	"testing"
+)
+
+func TestUserModelExists(t *testing.T) {
+	tests := []struct {
+		name   string
+		userId int
+		want   bool
+	}{
+		{
+			name:   "Valid ID",
+			userId: 1,
+			want:   true,
+		},
+		{
+			name:   "Zero ID",
+			userId: 0,
+			want:   false,
+		},
+		{
+			name:   "Non-existent ID",
+			userId: 2,
+			want:   false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := newTestDb(t)
+			m := UserModel{DB: db}
+
+			exists, err := m.Exists(tt.userId)
+
+			assert.Equal(t, exists, tt.want)
+			assert.NilError(t, err)
+		})
+	}
+}
