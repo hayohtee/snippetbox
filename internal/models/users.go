abb4a6463efc4105db6a6889246f0950ac6f8ce2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-05-30 14:05:50 +0100

    Add Get method to the interface and implement the Get method

--- internal/models/users.go
+++ internal/models/users.go
@@ -22,6 +22,7 @@ type UserModelInterface interface {
 	Insert(name, email, password string) error
 	Authenticate(email, password string) (int, error)
 	Exists(id int) (bool, error)
+	Get(id int) (*User, error)
 }
 
 // UserModel a type which wraps database connection pool.
@@ -87,4 +88,17 @@ func (m *UserModel) Exists(id int) (bool, error) {
 	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
 	err := m.DB.QueryRow(stmt, id).Scan(&exists)
 	return exists, err
+}
+
+func (u *UserModel) Get(id int) (*User, error) {
+	var user User
+	stmt := "SELECT id, name, email, created FROM users WHERE id = ?"
+	err := u.DB.QueryRow(stmt, id).Scan(&user.ID, &user.Name, &user.Email, &user.Created)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		}
+		return nil, err
+	}
+	return &user, nil
 }
