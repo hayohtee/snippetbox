f2ee9c2999b7313a38f7094f75716375a53b5b37
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-01 11:57:17 +0100

    Add new method to UserModelInterface and implement the interface method

--- internal/models/users.go
+++ internal/models/users.go
@@ -23,6 +23,7 @@ type UserModelInterface interface {
 	Authenticate(email, password string) (int, error)
 	Exists(id int) (bool, error)
 	Get(id int) (*User, error)
+	PasswordUpdate(id int, currentPassword, newPassword string) error
 }
 
 // UserModel a type which wraps database connection pool.
@@ -90,10 +91,10 @@ func (m *UserModel) Exists(id int) (bool, error) {
 	return exists, err
 }
 
-func (u *UserModel) Get(id int) (*User, error) {
+func (m *UserModel) Get(id int) (*User, error) {
 	var user User
 	stmt := "SELECT id, name, email, created FROM users WHERE id = ?"
-	err := u.DB.QueryRow(stmt, id).Scan(&user.ID, &user.Name, &user.Email, &user.Created)
+	err := m.DB.QueryRow(stmt, id).Scan(&user.ID, &user.Name, &user.Email, &user.Created)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrNoRecord
@@ -101,4 +102,31 @@ func (u *UserModel) Get(id int) (*User, error) {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (m *UserModel) PasswordUpdate(id int, currentPassword, newPassword string) error {
+	var currentHashedPassword []byte
+	stmt := "SELECT hashed_password FROM users WHERE id = ?"
+	err := m.DB.QueryRow(stmt, id).Scan(&currentHashedPassword)
+	if err != nil {
+		return err
+	}
+
+	err = bcrypt.CompareHashAndPassword(currentHashedPassword, []byte(currentPassword))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return ErrInvalidCredentials
+		} else {
+			return err
+		}
+	}
+
+	newHashedPassword, err := bcrypt.GenerateFromPassword([]byte(newPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt = "UPDATE users SET hashed_password = ? WHERE id = ?"
+	_, err = m.DB.Exec(stmt, string(newHashedPassword), id)
+	return err
 }
