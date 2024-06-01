fe5bd6c05d53cf7d835a04a34e2e5fb7db78c0cd
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-06-01 12:16:54 +0100

    Update the mock user to implement the interface

--- internal/models/mocks/users.go
+++ internal/models/mocks/users.go
@@ -44,3 +44,13 @@ func (u *UserModel) Get(id int) (*models.User, error) {
 	}
 	return nil, models.ErrNoRecord
 }
+
+func (u *UserModel) PasswordUpdate(id int, currentPassword, newPassword string) error {
+	if id == 1 {
+		if currentPassword != "pa$$word" {
+			return models.ErrInvalidCredentials
+		}
+		return nil
+	}
+	return models.ErrNoRecord
+}
